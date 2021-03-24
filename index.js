f08dbd1f8ffed41392c51e15462c928bcdadefb2
Ryan Helm <rjhelm@github.io>
2021-03-23 22:25:45 -0600

    added the remainder of questions for user input

---
+++ index.js
@@ -0,0 +1,75 @@
+// Project Variables 
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+const generate = require('./utils/generate');
+
+// User prompt functions
+const promptUser = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Briefly describe your project: '
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Include any installation steps: '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Describe the uses for the project: '   
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project: ',
+            choices: [
+                'Academic',
+                'Apache',
+                'Artistic',
+                'Boost',
+                'Creative',
+                'Educational',
+                'Eclipse',
+                'GNU',
+                'Microsoft',
+                'MIT',
+                'Mozilla',
+                'Open'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who were the main contributors for this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Are you including a test?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What if I have questions?'
+        },
+        {
+            type: 'input',
+            name: 'gitname',
+            message: 'Enter your github username: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address: '
+        }
+    ]);
+}
